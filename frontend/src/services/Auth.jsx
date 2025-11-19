4682080c822558c139e3cbbeb13f22da711be7cb
Olivanaa <oliv.anaclara@gmail.com>
2025-11-19 15:50:01 -0300

    estrutura e configuração do frontend, com paginas, rotas e autentificação

---
+++ frontend/src/services/Auth.jsx
@@ -0,0 +1,31 @@
+import { Navigate } from "react-router-dom"
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+export const isAuthenticated = () => {
+    return localStorage.getItem("token") !== null
+}
+
+export const getLoggedUser = () => {
+    const usuario = localStorage.getItem("usuario")
+    return usuario ? JSON.parse(usuario) : null
+}
+
+export const logout = () => {
+    localStorage.removeItem("token")
+    localStorage.removeItem("usuario")
+    window.location.href = "/"
+}
+
+export const fetchLoggedUser = async (userId) => {
+    const response = await fetch(`${API_URL}/usuarios/${userId}`)
+    if (!response.ok) throw new Error("Erro ao buscar usuário")
+    return await response.json()
+}
+
+export default function PrivateRoute({children}){
+    if (!isAuthenticated()) {
+        return <Navigate to="/login" replace />
+    }
+    return children
+}
