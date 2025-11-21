cc4a102e51674374f3b63ecbce125a91f4102fe9
Olivanaa <oliv.anaclara@gmail.com>
2025-11-21 18:12:18 -0300

    fix: arrumar erro de user ou token undefined

--- frontend/src/services/Auth.jsx
+++ frontend/src/services/Auth.jsx
@@ -2,11 +2,25 @@ import { Navigate } from "react-router-dom"
 
 const API_URL = import.meta.env.VITE_API_URL;
 
+const cleanInvalidStorage = () => {
+    const token = localStorage.getItem("token")
+    const user = localStorage.getItem("user")
+
+    if (token === "undefined" || token === "null" || !token) {
+        localStorage.removeItem("token")
+    }
+    if (user === "undefined" || user === "null" || !user) {
+        localStorage.removeItem("user")
+    }
+}
+
 export const isAuthenticated = () => {
+    cleanInvalidStorage()
     return localStorage.getItem("token") !== null
 }
 
 export const getLoggedUser = () => {
+    cleanInvalidStorage()
     const usuario = localStorage.getItem("user")
     return usuario ? JSON.parse(usuario) : null
 }
@@ -22,7 +36,7 @@ export const fetchLoggedUser = async (userId) => {
     return await response.json()
 }
 
-export default function PrivateRoute({children}){
+export default function PrivateRoute({ children }) {
     if (!isAuthenticated()) {
         return <Navigate to="/login" replace />
     }
